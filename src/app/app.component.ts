b42a70864d31eb7b0cb16264c5acf8d584b919b0
Yigit Can Dündar <canduendar@hotmail.com>
2018-01-22 10:16:01 +0100

    wichtige Dateien hinzugefügt

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = "app";
+}
