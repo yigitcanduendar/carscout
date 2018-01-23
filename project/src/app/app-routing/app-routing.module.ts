1c4c5855cae243ad57907c188439d1d78d8bb88d
Yigit Can Dündar <canduendar@hotmail.com>
2018-01-23 12:47:12 +0100

    #29 Login Component erstellt und Ordnerstruktur geändert

--- project/src/app/app-routing/app-routing.module.ts
+++ project/src/app/app-routing/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { CommonModule } from '@angular/common';
-import { TestComponent } from '../test/test.component';
 import { HomeComponent } from '../home/home.component';
 
 // Routing-----------------------------------------------
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'test', component: TestComponent },
 ];
 // Ende---------------------------------------------------
 
