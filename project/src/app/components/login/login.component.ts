8f71f2acd4b0e09f128df781cc91d98dee74fa56
Yigit Can Dündar <canduendar@hotmail.com>
2018-01-23 14:14:43 +0100

    #30 refactoring und auswerten des loginformulars

--- project/src/app/components/login/login.component.ts
+++ project/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public username;
+
   constructor() { }
+
+  save() {
+    console.log(this.username);
+  }
 
   ngOnInit() {
   }
