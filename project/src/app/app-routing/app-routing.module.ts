8c44c0ba044cfaa7d079c1227c6fa346690464d7
Yigit Can Dündar <canduendar@hotmail.com>
2018-01-23 13:06:40 +0100

    #29 refactoring

--- project/src/app/app-routing/app-routing.module.ts
+++ project/src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home/home.component';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
 
 // Routing-----------------------------------------------
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
 ];
 // Ende---------------------------------------------------
 
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
