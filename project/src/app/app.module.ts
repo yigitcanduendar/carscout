c76e0e63c10e2a9e43ee69df1f68721a74f30d6b
Yigit Can Dündar <canduendar@hotmail.com>
2018-01-23 14:52:13 +0100

    #30 Login Beispiel Logik umgebaut (DB fehlt)

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HomeComponent } from './components/home/home.component';
@@ -14,7 +14,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/components/login/login.component.ts
+++ project/src/app/components/login/login.component.ts
@@ -9,11 +9,39 @@ import { User } from '../../model/user';
 export class LoginComponent implements OnInit {
 
   public username;
+  public password;
+
+  /** Dummy-Daten zur Zeit nur als Objekt später von der DB! */
+  public users: Object = {
+    Can: 'test123',
+    Milan: 'test123'
+  };
 
   constructor() { }
 
+  /**
+   * Speichert und vergleicht ob Benutzer in DB existiert,
+   * wenn ja dann leite weiter und wenn nicht dann return zur Login-Seite.
+   */
   save() {
-    console.log(this.username);
+    if (this.checkUser(this.username, this.password)) {
+      console.log('verfügbar');
+    } else {
+      console.log('nicht verfügbar');
+    }
+  }
+
+  /**
+   * Überprüft, ob eingegebner User im Objekt ist.
+   * Später mit DB Abfrage!
+   */
+  public checkUser(username, password): Boolean {
+    for (const key in this.users) {
+      if (this.users.hasOwnProperty(this.username) && this.users[this.username] === this.password) {
+        return true;
+      }
+    }
+    return false;
   }
 
   ngOnInit() {
