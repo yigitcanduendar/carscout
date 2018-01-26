eaf96da1763b55db29b18bfbcb5564ee6e38f245
Milan Skripalle <milan102@szut.uni-bremen.de>
2018-01-26 10:55:32 +0100

    #22 Footer hinzugefügt, MessageBox abgeändert, Impressum, AGB, Datenschutzerklärung hinzugefügt

--- project/src/app/app-routing/app-routing.module.ts
+++ project/src/app/app-routing/app-routing.module.ts
@@ -4,11 +4,17 @@ import { AppComponent } from '../app.component';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
+import { AgbComponent } from '../components/legal-information/agb/agb.component';
+import { ImpressumComponent } from '../components/legal-information/impressum/impressum.component';
+import { PrivacyPolicyComponent } from '../components/legal-information/privacy-policy/privacy-policy.component';
 
 // Routing-----------------------------------------------
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'impressum', component: ImpressumComponent},
+  { path: 'privacyPolicy', component: PrivacyPolicyComponent},
+  { path: 'agb', component: AgbComponent}
 ];
 // Ende---------------------------------------------------
 
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MessageProviderService } from './services/messageprovider.service';
 import { MessageboxComponent } from './components/messagebox/messagebox.component';
+import { ImpressumComponent } from './components/legal-information/impressum/impressum.component';
+import { AgbComponent } from './components/legal-information/agb/agb.component';
+import { PrivacyPolicyComponent } from './components/legal-information/privacy-policy/privacy-policy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    MessageboxComponent
+    MessageboxComponent,
+    ImpressumComponent,
+    AgbComponent,
+    PrivacyPolicyComponent
   ],
   imports: [
     BrowserModule,
