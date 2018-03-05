e3ff813594a9b2cd29bdb13f1c3fd3c6efae9702
Milan Skripalle <milan102@szut.uni-bremen.de>
2018-03-05 12:59:16 +0100

    refactored

--- project/src/app/components/register/register.component.ts
+++ project/src/app/components/register/register.component.ts
@@ -11,42 +11,48 @@ import { MessageType } from '../../model/messagetype.enum';
 export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
+ 
   constructor(private messageService: MessageProviderService) {
-
   }
 
   public benutzer: User = new User();
   public cb_agb: boolean = false;
   public password2: string = "";
 
-  checkValid() {
+  submit(){
+    if(this.inputIsValid()){
+      //TODO
+    }else{
+      this.showMessage();
+    }
+  }
 
+  inputIsValid() {
     if (this.benutzer.name == null) {
       this.messageService.display("Bitte geben sie ein Benutzernamen ein.", MessageType.warning);
-      return;
+      return false;
     }
     if (this.benutzer.email == null) {
       this.messageService.display("Bitte geben sie ein gültige E-Mail Adresse ein.", MessageType.warning);
-      return;
+      return false;
     }
     if (this.benutzer.password == null) {
       this.messageService.display("Bitte geben sie ein Passwort ein.", MessageType.warning);
-      return;
+      return false;
     }
     if (this.password2 == null) {
       this.messageService.display("Bitte wiederholen sie Ihr Passwort.", MessageType.warning);
-      return;
+      return false;
     }
+  }
 
+  showMessage(){
     if (this.benutzer.name.length <= 3 || this.benutzer.email.length <= 4 || this.benutzer.password.length <= 4) {
       this.messageService.display("Bitte überprüfen Sie Ihre eingaben!", MessageType.warning);
     } else if (this.benutzer.password != this.password2) {
       this.messageService.display("Bitte überprüfen Sie Ihre eingaben!" + '<br/>' + "-Passwörter Stimmen nicht überein", MessageType.warning);
-    }
-    else {
+    } else {
       this.messageService.display("success?", MessageType.success);
     }
-
   }
-
 }
