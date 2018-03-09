72249a466c4b0e1459b71ed99fed6b0c501218d4
Yigit Can Dündar <canduendar@hotmail.com>
2018-03-09 09:07:12 +0100

    test

--- project/src/app/model/car.ts
+++ project/src/app/model/car.ts
@@ -13,6 +13,6 @@ export class Car {
         public category: string,
         public fuel_type: string,
         public state: string,
-        public price: string
+        public pricde: string
     ) { }
 }
