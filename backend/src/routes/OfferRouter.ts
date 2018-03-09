8511ba7c1e3e254542e6ac50163aabdc3de952c9
Daniel Peters <daniel1876@szut.uni-bremen.de>
2018-03-09 11:06:08 +0100

    #81 Funktion im OfferRouter zum Bild speichern, mit Überprüfung,
    Kommentar hinzugefügt

--- backend/src/routes/OfferRouter.ts
+++ backend/src/routes/OfferRouter.ts
@@ -21,6 +21,43 @@ export class OfferRouter {
   init() {
     this.router.get('/', this.getAll);
   }
+  /**
+   * Prüft die übergebenen Variablen, gibt bei Fehlern String zurück oder wenn
+   * in Ordnung speichert ein Bild per OfferDAO in der Datenbank ab und gibt 200 Ok als String zurück
+   * @param offer_id Id des Angebots, zu dem das Bild gehört
+   * @param pictureNr es kann eines von 5 Bildern sein, also Wert (1-5), ist für DB Spalte wichtig
+   * @param file die Bilddatei
+   */
+  static async saveImageForOffer(offer_id: number, pictureNr: number, file: File): Promise<string> {
+    let pictureNrString: String;
+    let filetype: String;
+    if (offer_id < 0) {
+      return "400 Bad Request. Das ist keine echte Id eines Angebots";
+
+    }
+    if (1 <= pictureNr && pictureNr <= 5) {
+      pictureNrString = pictureNr.toString();
+    } else {
+      return "400 Bad Request. Picture Nr war keine zulässige Zahl, (nicht zwischen 1 und 5)";
+    }
+
+    if (file) {
+      filetype = file.type;
+      var allowedFiletypes: String[] = ["jpg", "jpeg", "png", "bmp"];
+      if (allowedFiletypes.indexOf(filetype) == -1) {
+        return "406 Not Acceptable. Die Datei muss einen der folgenden Typen haben " + allowedFiletypes.toString;
+      }
+
+    } else {
+      return "400 Bad Request. File nicht vorhanden /  nicht lesbar ";
+    }
+
+
+    if (!OfferDAO.saveImageForOffer(offer_id, pictureNrString, file, filetype)) {
+      return "400 BadRequest. Speichern schlug fehl.";
+    }
+    return "200 Ok. Image saved succesfully";
+  }
 }
 
 const carRoutes = new OfferRouter();
