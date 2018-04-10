dabe96b9ede62c0863e925431db1fa54b4d31e9b
Milan <milan_skripalle@yahoo.de>
2018-04-10 16:14:09 +0200

    #24 MD5Hashing hinzugefügt

--- backend/src/dao/UserDAO.ts
+++ backend/src/dao/UserDAO.ts
@@ -1,4 +1,5 @@
 import { User } from '../model/User';
+import { Md5 } from 'ts-md5/dist/md5';
 const sqlite = require('sqlite-async');
 
 export class UserDAO {
@@ -22,7 +23,7 @@ export class UserDAO {
     static async insertNewUser(newUser : User){       
         let db = await sqlite.open(UserDAO.dbFile);
 
-        let user = await db.run("INSERT INTO Users (username, email, pw) VALUES('"+newUser.username+"','"+newUser.email+"','"+newUser.password+"')");
+        let user = await db.run("INSERT INTO Users (username, email, pw) VALUES('"+newUser.username+"','"+newUser.email+"','"+Md5.hashStr(newUser.password)+"')");
         db.close();
         
     return user;
